e3d6a4889db29a73f5a0fa7217a7757fcd098dec
Scott <ssk_8189@yahoo.com>
2016-12-31 00:07:06 -0500

    skip list initial commit with some formatting changes

---
+++ src/SkipListNode.java
@@ -0,0 +1,32 @@
+package src;
+
+/******************************************************************************
+ * src.SkipListNode                                                                *
+ *                                                                             *
+ * View README file for information about this project.                        *
+ * View LICENSE file for license information.                                  *
+ ******************************************************************************/
+
+import java.util.*;
+
+public class SkipListNode<E> {
+    private E value;
+    public List<SkipListNode<E>> nextNodes;
+
+    public SkipListNode(E value) {
+        this.value = value;
+        nextNodes = new ArrayList<>();
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public int level() {
+        return nextNodes.size() - 1;
+    }
+
+    public String toString() {
+        return "SLN: " + value;
+    }
+}

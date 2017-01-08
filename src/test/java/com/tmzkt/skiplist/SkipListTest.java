13ec652ead5e5ba3a511a209606be2de76b2cf38
Scott <ssk_8189@yahoo.com>
2017-01-08 11:59:56 -0500

    more unit tests

--- src/test/java/com/tmzkt/skiplist/SkipListTest.java
+++ src/test/java/com/tmzkt/skiplist/SkipListTest.java
@@ -2,6 +2,9 @@ package com.tmzkt.skiplist;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.*;
 
 public class SkipListTest {
@@ -29,5 +32,34 @@ public class SkipListTest {
 
         assertEquals(false, list.add(1));
         assertEquals(1, list.size());
+    }
+
+    @Test
+    public void itemsAreSorted() {
+        SkipList<Integer> list = new SkipList<>();
+        list.add(4);
+        list.add(2);
+        list.add(3);
+        List<Integer> orderedList = list.stream().collect(Collectors.toList());
+        assertEquals(2, (int) orderedList.get(0));
+        assertEquals(3, (int) orderedList.get(1));
+        assertEquals(4, (int) orderedList.get(2));
+    }
+
+    @Test
+    public void contains() {
+        SkipList<Integer> list = new SkipList<>();
+        list.add(3);
+        assertTrue(list.contains(3));
+        assertFalse(list.contains(2));
+    }
+
+    @Test // TODO make this test pass
+    public void remove() {
+        SkipList<Integer> list = new SkipList<>();
+        list.add(3);
+        assertTrue(list.contains(3));
+        list.remove(3);
+        assertFalse(list.contains(3));
     }
 }
